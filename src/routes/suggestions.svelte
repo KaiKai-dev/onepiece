475af1ed47fc1c44e4c82b5bfa56221b87a166ec
KaiKai-dev <jecalaycay17@gmail.com>
2022-02-23 18:02:42 +0800

    added lorem in suggestions

--- src/routes/suggestions.svelte
+++ src/routes/suggestions.svelte
@@ -4,7 +4,7 @@
 <main>
     <section class="index-banner">
       <div class="vertical-center">
-        <!-- put your content HERE -->
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio quas voluptatem cupiditate nisi laboriosam nam pariatur totam facilis voluptas! Officia accusamus saepe corrupti sunt alias!</p>
       </div>
     </section>
   </main>
